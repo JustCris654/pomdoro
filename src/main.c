ce63b70cfe888ab078dd4cf733f843ea44a6a140
Cristian Scapin <cristian.scapin654@icloud.com>
2025-08-27 21:36:39 +0200

    reset pomodoro timer

--- src/main.c
+++ src/main.c
@@ -22,8 +22,9 @@
 
 Vector2 textboxSizeForText(const char *text, int padding);
 void *startPomodoroTimer(void *arg);
-void drawStartPomodoro(bool *start);
 void drawTimer();
+void drawPomodoroBtn(const char *text, int x_quarters, int y_quarters,
+                     int padding, bool *clicked);
 
 typedef struct {
   int m;
@@ -33,6 +34,7 @@ typedef struct {
 typedef struct {
   int elapsedFromStart;
   bool isPomodoroRunning;
+  bool plsExit;
   Timer t;
 } PomodoroState;
 
@@ -43,6 +45,7 @@ int main(int argc, char **argv) {
   (void)argv;
 
   bool startPomodoro = false;
+  bool stopPomodoro = false;
   PomodoroState pState = {0, 0};
   pthread_t timerThread;
 
@@ -62,17 +65,26 @@ int main(int argc, char **argv) {
       pthread_mutex_lock(&mtx);
 
       if (!pState.isPomodoroRunning) {
-        drawStartPomodoro(&startPomodoro);
+        drawPomodoroBtn("#150#Start pomodoro", 2, 2, 150, &startPomodoro);
       } else {
-        printf("\r%02d:%02d", pState.t.m, pState.t.s);
-        fflush(stdout);
-        Vector2 textboxSize = textboxSizeForText("00000", 10); // it is always 5 characters
+        // DRAW TIMER
+        GuiSetStyle(DEFAULT, TEXT_SIZE, 96);
+        Vector2 textboxSize =
+            textboxSizeForText("000000", 10); // it is always 5 characters
         GuiLabel((Rectangle){floor(W_WIDTH / 2.0) - textboxSize.x / 2,
                              floor(W_HEIGHT / 2.0) - textboxSize.y / 2,
                              textboxSize.x, textboxSize.y},
-                 TextFormat("%02d:%02d", pState.t.m, pState.t.s));
+                 TextFormat("#219#%02d:%02d", pState.t.m, pState.t.s));
+        GuiSetStyle(DEFAULT, TEXT_SIZE, 48);
+
+        // DRAW STOP BUTTON
+        drawPomodoroBtn("#211#Stop", 3, 3, 10, &stopPomodoro);
       }
 
+      if (stopPomodoro) {
+        pState.plsExit = true;
+        stopPomodoro = false;
+      }
       pthread_mutex_unlock(&mtx);
 
       if (startPomodoro) {
@@ -106,6 +118,15 @@ void *startPomodoroTimer(void *arg) {
 
   while (seconds > 0) {
     pthread_mutex_lock(&mtx);
+
+    if (pState->plsExit) {
+      pState->isPomodoroRunning = false;
+      pState->plsExit = false;
+      pState->elapsedFromStart += 25 * 60 - seconds;
+      pthread_mutex_unlock(&mtx);
+      return NULL;
+    }
+
     pState->t.m = (seconds % (60 * 60)) / 60;
     pState->t.s = seconds % 60;
 
@@ -120,16 +141,25 @@ void *startPomodoroTimer(void *arg) {
 
     seconds -= 1;
   }
+
+  pthread_mutex_lock(&mtx);
+  pState->isPomodoroRunning = false;
+  pState->plsExit = false;
+  pState->elapsedFromStart += 25 * 60;
+  pthread_mutex_unlock(&mtx);
+
+  return NULL;
 }
 
-void drawStartPomodoro(bool *start) {
-  const char *text = "#150#Start pomodoro";
-  Vector2 textboxSize = textboxSizeForText(text, 150);
-  if (GuiButton((Rectangle){floor(W_WIDTH / 2.0) - textboxSize.x / 2,
-                            floor(W_HEIGHT / 2.0) - textboxSize.y / 2,
-                            textboxSize.x, textboxSize.y},
-                text)) {
-    *start = true;
+void drawPomodoroBtn(const char *text, int x_quarters, int y_quarters,
+                     int padding, bool *clicked) {
+  Vector2 textboxSize = textboxSizeForText(text, padding);
+  if (GuiButton(
+          (Rectangle){floor((W_WIDTH / 4.0) * x_quarters) - textboxSize.x / 2,
+                      floor((W_HEIGHT / 4.0) * y_quarters) - textboxSize.y / 2,
+                      textboxSize.x, textboxSize.y},
+          text)) {
+    *clicked = true;
   }
 }
 
